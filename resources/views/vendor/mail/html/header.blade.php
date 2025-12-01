5fd591b337803576da1bfa7c952f78675a4d1bab
RahmatBayu18 <b.anggoroyudo@gmail.com>
2025-12-02 04:39:20 +0700

    Fix logo in email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,15 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('aset/logo-PensHelpDes1.svg') }}" class="logo" alt="PensHelpDesk Logo" style="height: 60px; width: auto;">
-@else
-{!! $slot !!}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            {{--
+                Menggunakan asset() akan menghasilkan URL lengkap (misal: https://domainmu.com/aset/logo.svg).
+                Ini wajib untuk email karena Gmail tidak bisa membaca path relative.
+            --}}
+            <img src="{{ asset('aset/logo-PensHelpDes1.svg') }}" 
+                 class="logo" 
+                 alt="{{ config('app.name') }}" 
+                 style="height: 60px; width: auto; border: 0;">
+        </a>
+    </td>
 </tr>
