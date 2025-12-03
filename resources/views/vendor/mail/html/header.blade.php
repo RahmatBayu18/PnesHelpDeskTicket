7561c36d6753254cde93f60a0fe909f2249f968b
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2025-12-03 23:07:28 +0700

    fix: logo email notification

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('aset/logo-PensHelpDes1.svg') }}" 
+            <img src="{{ asset('aset/Logo-PensHelpDes.png') }}" 
                  class="logo" 
                  alt="{{ config('app.name') }}" 
                  style="height: 60px; width: auto; border: 0;">
